8f85b663ba192896cc03868af4c0a6eca73a5f49
anela <anela89@gmail.com>
2022-07-06 10:58:21 -0700

    Updated config.testing for non-mac use.

--- config.js
+++ config.js
@@ -4,16 +4,25 @@
 
 require("dotenv").config();
 require("colors");
+const { DATABASE_TEST, DATABASE } = require("./.env");
+
 
 const SECRET_KEY = process.env.SECRET_KEY || "secret-dev";
 
 const PORT = +process.env.PORT || 3001;
 
 // Use dev database, testing database, or via env var, production database
+// function getDatabaseUri() {
+//   return (process.env.NODE_ENV === "test")
+//       ? "jobly_test"
+//       : process.env.DATABASE_URL || "jobly";
+// }
+
+// Use dev database, testing database, or via env var, production database
 function getDatabaseUri() {
   return (process.env.NODE_ENV === "test")
-      ? "jobly_test"
-      : process.env.DATABASE_URL || "jobly";
+      ? DATABASE_TEST
+      : process.env.DATABASE_URL || DATABASE;
 }
 
 // Speed up bcrypt during tests, since the algorithm safety isn't being tested
--- config.test.js
+++ config.test.js
@@ -18,10 +18,11 @@ describe("config can come from env", function () {
     delete process.env.BCRYPT_WORK_FACTOR;
     delete process.env.DATABASE_URL;
 
-    expect(config.getDatabaseUri()).toEqual("jobly");
+    expect(config.getDatabaseUri()).not.toContain("_test");
+    expect(config.getDatabaseUri()).toContain("jobly");
+    
     process.env.NODE_ENV = "test";
 
-    expect(config.getDatabaseUri()).toEqual("jobly_test");
+    expect(config.getDatabaseUri()).toContain("jobly_test");
   });
 })
-
