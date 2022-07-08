dcfef8cd9fdd2b84aed6a20966915d5a9867a043
robin <robinnsheen@berkeley.edu>
2022-07-08 11:53:41 -0700

    configs

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ const { DATABASE_TEST, DATABASE } = require("./.env");
 
 const SECRET_KEY = process.env.SECRET_KEY || "secret-dev";
 
-const PORT = +process.env.PORT || 3001;
+const PORT = +process.env.PORT || 3000;
 
 // Use dev database, testing database, or via env var, production database
 // function getDatabaseUri() {
@@ -21,8 +21,8 @@ const PORT = +process.env.PORT || 3001;
 // Use dev database, testing database, or via env var, production database
 function getDatabaseUri() {
   return (process.env.NODE_ENV === "test")
-      ? DATABASE_TEST
-      : process.env.DATABASE_URL || DATABASE;
+  ? "jobly_test"
+  : process.env.DATABASE_URL || "jobly";
 }
 
 // Speed up bcrypt during tests, since the algorithm safety isn't being tested
--- server.js
+++ server.js
@@ -3,6 +3,6 @@
 const app = require("./app");
 const { PORT } = require("./config");
 
-app.listen(PORT, function () {
-  console.log(`Started on http://localhost:${PORT}`);
+app.listen(process.env.PORT, function() {
+  console.log("Server starting!");
 });
